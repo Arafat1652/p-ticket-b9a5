00ba8ddb1f6008b42a41133277f08dee02b77865
Arafat1652 <arafathosen4042@gmail.com>
2024-02-22 06:47:45 -0800

    trying dom

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+const seatName = ['1', '2'];
+function makeArrayOfSeat(arr){
+    return arr;
+}
+const myArray =makeArrayOfSeat(seatName)
+console.log('arr')
+
+const findId = document.getElementById('seat-holder')
+console.log(findId)
